77d02685bed7552d7d13fe0fe03831a4f871e27a
joana.socrates <joana.socrates@ocado.com>
2018-06-27 18:12:10 +0200

    start-Implementing-pricing

--- src/main/java/com/joanadantas/Pricing.java
+++ src/main/java/com/joanadantas/Pricing.java
@@ -1,4 +1,19 @@
 package com.joanadantas;
 
-public class Pricing {
+public abstract class Pricing {
+    private final double pricePerDay;
+    private final int numberOfFixedDays;
+
+    public Pricing (double pricePerDay, int numberOfFixedDays){
+        this.pricePerDay = pricePerDay;
+        this.numberOfFixedDays = numberOfFixedDays;
+    }
+
+    public double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public int getNumberOfFixedDays() {
+        return numberOfFixedDays;
+    }
 }
--- src/main/java/com/joanadantas/RegularPricing.java
+++ src/main/java/com/joanadantas/RegularPricing.java
@@ -1,4 +1,9 @@
 package com.joanadantas;
 
 public class RegularPricing extends Pricing {
+
+    public RegularPricing(){
+        super(1.0, 3);
+    }
+
 }
